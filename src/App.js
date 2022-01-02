11c34b7ca13c78c229c229b15fb39f4bc76c901d
Zhoomart Akimov <akimov_z@auca.kg>
2022-01-03 00:59:01 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
+// import React from "react";
+// import "./App.css";
+// import 'react-responsive-carousel/lib/styles/carousel.min.css';
+// import 'react-multi-carousel/lib/styles.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Home from "./pages/Home";
+// import Rooms from "./pages/Rooms";
+// import SingleRoom from "./pages/SingleRoom";
+// import Error from "./pages/Error";
+// import About from "./pages/About";
+
+// import Navbar from "./components/Navbar";
+
+// import SortedR from './pages/SortedR';
+
+// import { Switch, Route } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Switch>
+//         <Route exact path="/" component={Home} />
+//         <Route exact path="/rooms/" component={Rooms} />
+//         <Route exact path="/about/" component={About} />
+//         <Route exact path="/rooms/:slug" component={SingleRoom} />
+//         <Route path="/results-rooms/" component={SortedR} />
+//         <Route component={Error} />
+//       </Switch>
+//     </>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import "./App.css";
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import 'react-multi-carousel/lib/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
@@ -14,57 +51,31 @@ import Navbar from "./components/Navbar";
 
 import SortedR from './pages/SortedR';
 
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/rooms/" component={Rooms} />
-        <Route exact path="/about/" component={About} />
-        <Route exact path="/rooms/:slug" component={SingleRoom} />
-        <Route path="/results-rooms/" component={SortedR} />
-        <Route component={Error} />
-      </Switch>
+      <TransitionGroup>
+        <CSSTransition
+          timeout={250}
+          classNames="fade"
+          key={location.key}
+        >
+          <Switch location={location}>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/rooms/" component={Rooms} />
+            <Route exact path="/about/" component={About} />
+            <Route exact path="/rooms/:slug" component={SingleRoom} />
+            <Route exact path="/results-rooms/" component={SortedR} />
+            <Route component={Error} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
     </>
   );
 }
 
 export default App;
-
-
-
-// import React from "react";
-// import "./App.css";
-// import 'react-responsive-carousel/lib/styles/carousel.min.css';
-// import 'react-multi-carousel/lib/styles.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import Home from "./pages/Home";
-// import Rooms from "./pages/Rooms";
-// import SingleRoom from "./pages/SingleRoom";
-// import Error from "./pages/Error";
-
-// import Navbar from "./components/Navbar";
-// // import SortedR from './pages/SortedR';
-
-// import { Switch, Route } from "react-router-dom";
-
-// function App() {
-//   return (
-//     <>
-//       <Navbar />
-//       <Switch>
-//         <Route exact path="/" component={Home} />
-//         <Route path="/rooms/" component={Rooms} />
-//         {/* <Route path="/sorted-rooms/" component={SortedR} /> */}
-//         <Route path="/rooms/:slug" component={SingleRoom} />
-//         <Route path="*" component={Error} />
-//       </Switch>
-//     </>
-//   );
-// }
-
-// export default App;
